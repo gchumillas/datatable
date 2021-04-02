e1b137be5f1c25d1a8c5eb4859736fd6055bc333
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-04-02 20:08:57 +0200

    add refresh button

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { useAsync } from 'react-use'
-import { CssBaseline, Container } from '@material-ui/core'
+import { useAsyncRetry } from 'react-use'
+import { CssBaseline, Container, IconButton } from '@material-ui/core'
+import { Refresh as RefreshIcon } from '@material-ui/icons'
 import DataTable from '@plastic-ui/datatable'
 import { Item, getItems } from './providers/items'
 
 export default () => {
   const [items, setItems] = React.useState<Item[]>([])
 
-  useAsync(async () => {
+  const { loading, retry } = useAsyncRetry(async () => {
     setItems(await getItems())
   }, [])
 
@@ -16,8 +17,22 @@ export default () => {
       <CssBaseline />
       <Container>
         <DataTable
+          loading={loading}
           title="List of items"
-          columns={[{ name: 'title', label: 'Title' }]}
+          columns={[
+            { name: 'title', label: 'Title' },
+            {
+              name: 'actions',
+              align: 'right',
+              label: (
+                <>
+                  <IconButton onClick={retry}>
+                    <RefreshIcon />
+                  </IconButton>
+                </>
+              )
+            }
+          ]}
           rows={items}
         />
       </Container>
